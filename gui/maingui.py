05627945e886c3f21ede1e16a38c59b40bf7a5b0
mirkat_pc <mirkat.ding@gmail.com>
2023-02-05 21:06:49 +0800

    Create menu, and add basic dialogs to open file/directory

--- gui/maingui.py
+++ gui/maingui.py
@@ -17,10 +17,49 @@ from PyQt5.QtWidgets import (
     QAction,
 )
 
+from liberty import Liberty
+
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.init_ui()
+        self.init_menu()
 
     def init_ui(self):
         self.setWindowTitle('LibertyViewer')
+    
+    def init_menu(self):
+        menu = self.menuBar()
+
+        # 'File'
+        file_menu = menu.addMenu('&File')
+        openfile_action = QAction('Open File', self)
+        openfile_action.triggered.connect(self._open_file_dialog)
+        file_menu.addAction(openfile_action)
+        opendirectory_action = QAction('Open Directory', self)
+        opendirectory_action.triggered.connect(self._open_directory_dialog)
+        file_menu.addAction(opendirectory_action)
+
+        # 'Plot'
+        plot_menu = menu.addMenu('&Plot')
+
+        # 'Help'
+        help_menu = menu.addMenu('&Help')
+
+    def _open_directory_dialog(self):
+        dirpath = QFileDialog.getExistingDirectory(
+            self,
+            caption='Open Directory',
+        )
+        if dirpath:
+            print(f'Loading directory: {dirpath}')
+
+    def _open_file_dialog(self):
+        filepath, _ = QFileDialog.getOpenFileName(
+            self,
+            caption='Open File',
+            filter='JSON Files (*.json)'
+        )
+        if filepath:
+            print(f'Loading file: {filepath}')
+            self.lib = Liberty(filepath=filepath)
