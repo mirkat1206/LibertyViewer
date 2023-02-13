18ee5cd2d07389533e4f15f32248345f2e45287b
mirkat_pc <mirkat.ding@gmail.com>
2023-02-13 11:44:56 +0800

    Add another control panel and a control plot button in plot_default.py

--- plotting/plot_default.py
+++ plotting/plot_default.py
@@ -7,7 +7,7 @@
 # @email: mirkat.ding@gmail.com       #
 # @date created: 2023/2/8             #
 # @last modified by: Shiuan-Yun Ding  #
-# @last modified date: 2023/2/9       #
+# @last modified date: 2023/2/13      #
 # ################################### #
 
 from random import randint
@@ -27,6 +27,8 @@ class PlotDefault(QWidget):
         self.init_ui()
         self.init_layout()
         self.init_control_panel()
+        self.maxn = 10
+        self.type = None
 
     def init_ui(self):
         # plotting
@@ -34,19 +36,32 @@ class PlotDefault(QWidget):
         self.plot_w.setBackground('w')
         self.plot_w.showGrid(x=True, y=True)
         self.plot_w.setTitle('Default')
-        # control panel
-        self.control_title = QLabel('Plot Control Panel')
-        self.control_list = QListWidget()
+        # control panel 1
+        self.control_1_title = QLabel('Plot Control Panel')
+        self.control_1_list = QListWidget()
+        # control panel 2
+        self.control_2_title = QLabel('Number Control Panel')
+        self.control_2_list = QListWidget()
+        #
+        self.control_plot = QPushButton('Plot')
         self.control_autorange = QPushButton('Auto Range')
 
     def init_layout(self):
         # control panel
         control_v = QVBoxLayout()
         control_v.setContentsMargins(0, 0, 0, 0)
-        control_v.addWidget(self.control_title)
-        self.control_title.setStyleSheet('font: bold')
-        control_v.addWidget(self.control_list)
+        # control panel 1
+        control_v.addWidget(self.control_1_title)
+        self.control_1_title.setStyleSheet('font: bold')
+        control_v.addWidget(self.control_1_list)
+        # control panel 2
+        control_v.addWidget(self.control_2_title)
+        self.control_2_title.setStyleSheet('font: bold')
+        control_v.addWidget(self.control_2_list)
+        #
+        control_v.addWidget(self.control_plot)
         control_v.addWidget(self.control_autorange)
+        #
         control_v_w = QWidget()
         control_v_w.setLayout(control_v)
         control_v_w.setFixedWidth(180)
@@ -58,12 +73,22 @@ class PlotDefault(QWidget):
         self.setLayout(layout_h)
 
     def init_control_panel(self):
-        self.control_list.addItems([
+        # control panel 1
+        self.control_1_list.addItems([
             'plain plot',
             'scatter plot',
             'line plot'
         ])
-        self.control_list.currentTextChanged.connect(self._handle_control_plot_clicked)
+        self.control_1_list.currentTextChanged.connect(self._handle_control_plot_1_clicked)
+        # control panel 2
+        self.control_2_list.addItems([
+            '10',
+            '100',
+            '1000'
+        ])
+        self.control_2_list.currentTextChanged.connect(self._handle_control_plot_2_clicked)
+        #
+        self.control_plot.clicked.connect(self._handle_control_plot_clicked)
         self.control_autorange.clicked.connect(self._handle_control_autorange_clicked)
 
     def _handle_control_autorange_clicked(self):
@@ -71,14 +96,21 @@ class PlotDefault(QWidget):
 
     def _handle_control_plot_clicked(self):
         self.plot_w.clear()
-        selected = self.control_list.currentItem().text()
-        if selected == 'plain plot':
+        if self.type == 'plain plot':
             self._display_plain_plot()
-        elif selected == 'scatter plot':
+        elif self.type == 'scatter plot':
             self._display_scatter_plot()
-        elif selected == 'line plot':
+        elif self.type == 'line plot':
             self._display_line_plot()
         self.plot_w.autoRange()
+
+    def _handle_control_plot_1_clicked(self):
+        self.type = self.control_1_list.currentItem().text()
+        print(f'selected type = {self.type}')
+
+    def _handle_control_plot_2_clicked(self):
+        self.maxn = int(self.control_2_list.currentItem().text())
+        print(f'selected maxn = {self.maxn}')
 
     def _display_plain_plot(self):
         print('Display default plot')
@@ -90,8 +122,8 @@ class PlotDefault(QWidget):
         scatter = pg.ScatterPlotItem(symbol='o', size=2)
         self.plot_w.addItem(scatter)
         n = 1000
-        x = [randint(0, 100) for _ in range(n)]
-        y = [randint(0, 100) for _ in range(n)]
+        x = [randint(0, self.maxn) for _ in range(n)]
+        y = [randint(0, self.maxn) for _ in range(n)]
         pens = [pg.mkPen(width=5, color=pg.intColor(randint(0, 1000))) for _ in range(n)]
         scatter.setData(x, y, pen=pens)
 
@@ -102,5 +134,5 @@ class PlotDefault(QWidget):
             pen = pg.mkPen(width=2, color=pg.intColor(randint(0, 1000)))
             n = 20
             x = [i for i in range(n)]
-            y = [randint(0, 100) for _ in range(n)]
+            y = [randint(0, self.maxn) for _ in range(n)]
             self.plot_w.plot(x, y, pen=pen, symbol='+')
