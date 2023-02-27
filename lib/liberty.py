678473ff0379a299f303682d826dd336fdae4eb8
mirkat_pc <mirkat.ding@gmail.com>
2023-02-27 16:13:02 +0800

    Fix bugs in liberty.py

--- lib/liberty.py
+++ lib/liberty.py
@@ -63,13 +63,13 @@ class Cell(Group):
         return [k for k, v in self.content['pin'].items() if v['direction'] == 'output']
 
     def get_output_pins(self):
-        return self.get_pins(self.list_input_pins())
+        return self.get_pins(self.list_output_pins())
 
     def list_inout_pins(self):
         return [k for k, v in self.content['pin'].items() if v['direction'] == 'inout']
 
     def get_inout_pins(self):
-        return self.get_pins(self.list_input_pins())
+        return self.get_pins(self.list_inout_pins())
     
 
 class Liberty(Group):
